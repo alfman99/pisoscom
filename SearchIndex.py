43932f38f1e8d5c76a030f45c44fdcd1c459e0a2
edux1 <edu.pm97@gmail.com>
2022-11-22 21:55:27 +0100

    Modificado para buscar indices de nuestras consultas

---
+++ SearchIndex.py
@@ -0,0 +1,62 @@
+"""
+.. module:: SearchIndex
+
+SearchIndex
+***********
+
+:Description: SearchIndex
+
+    Searches for a specific word in the field 'text' (--text)  or performs a query (--query) (LUCENE syntax,
+    between single quotes) in the documents of an index (--index)
+    
+
+:Version: 
+
+:Created on: 29/08/2022 
+
+"""
+from __future__ import print_function
+from elasticsearch import Elasticsearch
+from elasticsearch.exceptions import NotFoundError
+
+import argparse
+
+from elasticsearch_dsl import Search
+from elasticsearch_dsl.query import Q
+
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--index', default=None, required=True, help='Index to search')
+    parser.add_argument('--query', default=None, nargs=argparse.REMAINDER, help='Lucene query')
+
+    args = parser.parse_args()
+
+    index = args.index
+    if args.query:
+        query = ' '.join(args.query)
+
+    try:
+        client = Elasticsearch()
+        s = Search(using=client, index=index)
+
+        q = Q('query_string', query=query)
+        s = s.query(q)
+        response = s[0:10].execute()
+        #print("response:", response)
+
+        for r in response:
+            print('TITLE = %s URL =%s' % (r['title'], r['url']))
+            print('DIRECTION = %s' % r['direccion'])
+            print('DESCRIPTION = %s' % r['descripcion'])
+            print('PRICE = %s' % r['precio'])
+            print('ROOMS = %s' % r['nHabitaciones'])
+            print('M2 = %s' % r['metrosCuadrados'])
+            print('BATHROOMS = %s' % r['nBanyos'])
+            print('----------------------------------------')
+
+
+        print ('%d Documents'% response.hits.total.value)
+    except NotFoundError:
+        print('Index %s does not exist' % index)
+
